4dbd4b254e42485d837a4eba0c8e055a0a6a9c04
Huang Wei <huangwei@apache.org>
2024-02-29 10:42:53 +0800

    fix scala test

--- test/scala/src/main/scala/SimpleApp.scala
+++ test/scala/src/main/scala/SimpleApp.scala
@@ -10,16 +10,17 @@ object SimpleApp {
     println(spark.sessionState.catalog.listDatabases)
     spark.sql("show namespaces;").show()
     spark.sql("show tables;").show()
-    // spark.sql("select * from nyc_ice_hive.taxis").show()
+    spark.sql("select * from basic_test.acid").show()
     // spark.sql("show namespaces from hadoop_prod;").show()
     // acid table needs hive conf about txn, set it in hive-site.xml
-    // val df = spark.read.format("jdbc").option("driver", "org.apache.hive.jdbc.HiveDriver")
-    // // configuration variable
-    // // jdbc:hive2://<host1>:<port1>,<host2>:<port2>/dbName;initFile=<file>;sess_var_list?hive_conf_list#hive_var_list, it should be the hive_conf_list
-    //   // .option("url", "jdbc:hive2://hiveserver2:10000/;?hive.txn.manager=org.apache.hadoop.hive.ql.lockmgr.DbTxnManager") // it's right url, you can check `set;` on beeline, but org.apache.hive.service.cli.HiveSQLException: Error running query
-    //   // .option("hive.txn.manager", "org.apache.hadoop.hive.ql.lockmgr.DbTxnManager")
-    //   .option("dbtable", "basic_test.acid").load() // .option("user", "USER").option("password", "PWD").option("fetchsize","20").load()
-    // df.show()
+    val df = spark.read.format("jdbc").option("driver", "org.apache.hive.jdbc.HiveDriver")
+    // configuration variable
+    // jdbc:hive2://<host1>:<port1>,<host2>:<port2>/dbName;initFile=<file>;sess_var_list?hive_conf_list#hive_var_list, it should be the hive_conf_list
+    // .option("url", "jdbc:hive2://hiveserver2:10000/") // org.apache.hive.service.cli.HiveSQLException: Error while compiling statement: FAILED: SemanticException [Error 10265]: This command is not allowed on an ACID table basic_test.acid with a non-ACID transaction manager. Failed command: SELECT * FROM basic_test.acid WHERE 1=0
+      // .option("url", "jdbc:hive2://hiveserver2:10000/;?hive.txn.manager=org.apache.hadoop.hive.ql.lockmgr.DbTxnManager") // it's right url, you can check `set;` on beeline, but org.apache.hive.service.cli.HiveSQLException: Error running query
+      // .option("hive.txn.manager", "org.apache.hadoop.hive.ql.lockmgr.DbTxnManager")
+      .option("dbtable", "basic_test.acid").load() // .option("user", "USER").option("password", "PWD").option("fetchsize","20").load()
+    df.show()
     spark.stop()
   }
 }
